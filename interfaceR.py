87dd29aea069370b10fec29eab3596f2e2304d88
RoNaranjo02 <130687333+RoNaranjo02@users.noreply.github.com>
2023-06-07 23:42:35 -0300

    Add files via upload

---
+++ interfaceR.py
@@ -0,0 +1,15 @@
+from zope.interface import Interface
+
+class IAgente(Interface):
+
+    def insertarElemento(posicion):
+        pass
+        
+
+    def agregarElemento(elemento):
+        pass
+
+    def mostrarElemento(posicion):
+        pass
+
+
